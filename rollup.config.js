cdd26c44dc1ef53583e7259895c2f1dbc8a624e7
Jason Lee <jason@zerodevx.com>
2020-09-17 20:58:19 +0800

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,49 @@
 import svelte from 'rollup-plugin-svelte'
 import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
-import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
 
 const production = !process.env.ROLLUP_WATCH
 
-const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase())
-
-function serve() {
-	let server
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+function serve () {
+  let server
+  function toExit () {
+    if (server) server.kill(0)
+  }
+  return {
+    writeBundle () {
+      if (server) return
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      })
+      process.on('SIGTERM', toExit)
+      process.on('exit', toExit)
+    }
+  }
 }
 
-export default production
-? {
-		input: 'src/index.js',
-		output: [
-			{ file: pkg.module, format: 'es' },
-			{ file: pkg.main, format: 'umd', name }
-		],
-		plugins: [
-			svelte(),
-			resolve()
-		]
-	}
-: {
-		input: 'docs/main.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: 'docs/build/bundle.js'
-		},
-		plugins: [
-			svelte({
-				dev: true,
-				css: css => {
-					css.write('bundle.css');
-				}
-			}),
-			resolve(),
-			serve(),
-			livereload('docs')
-		],
-		watch: {
-			clearScreen: false
-		}
-	}
+export default {
+  input: 'docs/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'docs/build/bundle.js'
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: true
+    }),
+    resolve(),
+    commonjs(),
+    !production && serve(),
+    !production && livereload('docs'),
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
